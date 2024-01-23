13f712ce3d80f50dec55d6319631e04e367327f4
borisngong <borisngong@gmail>
2024-01-23 15:33:13 -0800

    Shell sort - Knuth Sequence

---
+++ 100-shell_sort.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * shell_sort - responsible for sorting an array of integers
+ * using Shell sort algorithm
+ * @array: represents the array to be sorted
+ * @size: represents the size of the array
+ */
+
+void shell_sort(int *array, size_t size)
+{
+	size_t gap = 1, i, j;
+	int temp;
+
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
+
+	for (; gap > 0; gap /= 3)
+	{
+		for (i = gap; i < size; i++)
+		{
+			temp = array[i];
+
+			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+			{
+				array[j] = array[j - gap];
+			}
+
+			array[j] = temp;
+		}
+
+		for (i = 0; i < size; i++)
+		{
+			printf("%d", array[i]);
+			if (i < size - 1)
+				printf(", ");
+		}
+		printf("\n");
+	}
+}
