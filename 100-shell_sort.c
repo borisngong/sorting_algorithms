4ba65064522f7b659b12e0a85d619bf971dc8040
borisngong <borisngong@gmail>
2024-01-23 15:59:33 -0800

    Shell sort - Knuth Sequence

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,48 +1,48 @@
 #include "sort.h"
 
 /**
- * int_swap - Swap two integers in an array.
+ * int_swap - responsible for swapping two integers in an array.
  * @a: The first integer to swap.
  * @b: The second integer to swap.
  */
 void int_swap(int *a, int *b)
 {
-    int tmp;
+	int tmp;
 
-    tmp = *a;
-    *a = *b;
-    *b = tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 /**
- * shell_sort - reponsible for sorting an array of integers in ascending
+ * shell_sort - responsible for sorting an array of integers in ascending
  * order using the shell sort algorithm.
- * @array: represents an array of integers.
- * @size: represents the size of the array.
+ * @array: An array of integers.
+ * @size: The size of the array.
  *
  * Description: Uses the Knuth interval sequence.
  */
 void shell_sort(int *array, size_t size)
 {
-    size_t interval, k, j;
+	size_t interval, k, j;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (interval = 1; interval < (size / 3);)
-        interval = interval * 3 + 1;
+	for (interval = 1; interval < (size / 3);)
+		interval = interval * 3 + 1;
 
-    for (; interval >= 1; interval /= 3)
-    {
-        for (k = interval; k < size; k++)
-        {
-            j = k;
-            while (j >= interval && array[j - interval] > array[j])
-            {
-                int_swap(array + j, array + (j - interval));
-                j -= interval;
-            }
-        }
-        print_array(array, size);
-    }
+	for (; interval >= 1; interval /= 3)
+	{
+		for (k = interval; k < size; k++)
+		{
+			j = k;
+			while (j >= interval && array[j - interval] > array[j])
+			{
+				int_swap(array + j, array + (j - interval));
+				j -= interval;
+			}
+		}
+		print_array(array, size);
+	}
 }
